92f4d8afbb23a5e78a6f8fcbd22c896aa2284913
Vette <stephane@triomphe.tf>
2026-06-22 21:57:58 +0200

    Deactivate flow control for BT uart as we get errors when module play (no possible to send pause command)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -619,7 +619,9 @@ static void MX_USART2_UART_Init(void)
     huart2.Init.StopBits = UART_STOPBITS_1;
     huart2.Init.Parity = UART_PARITY_NONE;
     huart2.Init.Mode = UART_MODE_TX_RX;
-    huart2.Init.HwFlowCtl = UART_HWCONTROL_RTS_CTS;
+    // Remove hardware flow control, as it timeouts commands in play mode
+    // So pause is not possible.
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
     huart2.Init.OverSampling = UART_OVERSAMPLING_16;
     if (HAL_UART_Init(&huart2) != HAL_OK)
     {
